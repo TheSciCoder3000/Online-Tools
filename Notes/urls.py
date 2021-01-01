c66fa163eb750c6e023eb41c9a841576cc0f0184
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-01-01 15:25:39 +0800

    changing navbar style

--- Notes/urls.py
+++ Notes/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dash, name='dashboard'),
     path('notes/', views.notes, name='notes'),
+
+    # Javascript function links
     path('update_notes/', views.update_notes, name='update-notes'),
     path('get_notes/', views.send_note_data, name='get-notes'),
     path('update_folders/', views.update_folder_tree, name='update-folders'),
